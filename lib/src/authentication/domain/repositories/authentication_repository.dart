7b6294dcc1810c1586f2d3593f65cb9ae8745744
toculin38 <toculin38@gmail.com>
2023-10-22 01:28:12 +0800

    add usecase

--- lib/src/authentication/domain/repositories/authentication_repository.dart
+++ lib/src/authentication/domain/repositories/authentication_repository.dart
@@ -1,14 +1,16 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_application_1/core/errors/failure.dart';
+import 'package:flutter_application_1/core/utils/typedef.dart';
 import 'package:flutter_application_1/src/authentication/domain/entities/user.dart';
 
 abstract class AuthoenticationRepository {
   const AuthoenticationRepository();
 
-  Future<Either<Exception, void>> createUser({
+  ResultVoid createUser({
     required String createAt,
     required String name,
     required String avatar,
   });
 
-  Future<Either<Exception, List<User>>> getUsers();
+  ResultFuture<List<User>> getUsers();
 }
---
+++ lib/src/authentication/domain/usecases/create_user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_application_1/core/utils/typedef.dart';
+import 'package:flutter_application_1/src/authentication/repositories/authentication_repository.dart';
+
+class CreateUser {
+  const CreateUser(this._repository);
+
+  final AuthoenticationRepository _repository;
+
+  ResultVoid createUser({
+    required String createAt,
+    required String name,
+    required String avatar,
+  }) async =>
+      _repository.createUser(createAt: createAt, name: name, avatar: avatar);
+}
